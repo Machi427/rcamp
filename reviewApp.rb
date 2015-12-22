8468bed1ab6d21f616051a089e14d6123a24fa40
machi <machi@v157-7-220-18.myvps.jp>
2015-12-22 11:06:06 +0900

    second commit

---
+++ reviewApp.rb
@@ -0,0 +1,12 @@
+genre = "映画"
+title = "時をかける少女"
+review = "人生の最高傑作アニメ。\n\n青春厨としてはたまらない甘酸っぱい青春ストーリー。\n\nちあきのイケメンさは言うまでもない。\n\n人にお金を払ってでもみて欲しい作品。\n\n早く見たほうがいいよ。\nTime waits for no one."
+line = "---------------------------"
+
+puts "ジャンル : " + genre
+puts line
+puts "タイトル : " + title
+puts line
+puts "感想 :"
+puts review
+puts line
