375f2b354bd42c076d728bb670908c73a9ade69c
Machi427 <machirda427@gmail.com>
2015-12-31 15:22:08 +0900

    html,css途中

--- sample_class.rb
+++ sample_class.rb
@@ -1,10 +1,44 @@
 class Review
+  @@review_count = 0
   def self.get_review_count
-    return 0
+    return @@review_count
+  end
+
+  def initialize
+    @@review_count = @@review_count + 1
+  end
+
+  def write_review
+    puts "タイトルを入力してください"
+    @title = gets.chomp
+    puts "ジャンルを入力してください"
+    @genre = gets.chomp
+    puts "感想を入力してください"
+    @impression = gets.chomp
+  end
+
+  def show_review
+    line = "---------------------------"
+    puts "ジャンル : #{@genre}\n#{line}\n"
+    puts "タイトル : #{@title}\n#{line}\n"
+    puts "感想 :\n#{@impression}\n#{line}\n"
   end
 end
 
-review = Review.new # Reviewクラスのインスタンスを生成
-puts review
+while true do
+  # メニューの表示
+  puts "書いたレビューの数：#{Review.get_review_count}"
+  puts "[0]レビューを書く"
+  puts "[1]アプリを終了"
+  input = gets.to_i
 
-puts Review.get_review_count
+  if input == 0    # レビューを書く
+    review = Review.new
+    review.write_review
+    review.show_review
+  elsif input == 1 # アプリを終了
+    exit
+  else             # その他の入力
+    puts "入力された値は無効な値です"
+  end
+end
